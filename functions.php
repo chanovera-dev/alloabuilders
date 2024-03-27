ba0cfd3b6c3cf2fef7b708e1fb0b3b305bf14739
Chano <github@chanovera.com>
2024-03-27 16:54:06 -0600

    desactivar duotone

--- functions.php
+++ functions.php
@@ -83,7 +83,6 @@ add_action("wp_footer", "ver_archivos_cargados");
 
 
 // Remove unwanted SVG filter injection WP
-remove_action( 'wp_enqueue_scripts', 'wp_enqueue_global_styles' );
 remove_action( 'wp_body_open', 'wp_global_styles_render_svg_filters' );
 /**
  * Disable the emoji's
