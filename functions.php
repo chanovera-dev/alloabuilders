7b8b91492dcae5dce0d84a54ef32780d0d539a9c
Chano <github@chanovera.com>
2024-03-27 16:41:59 -0600

    desactivando wp block librwry, wp polifill y regenerator runtime

--- functions.php
+++ functions.php
@@ -1,7 +1,11 @@
 <?php
 
 // carga componentes (estilos, javascript, etc) en el header
-function load_components_header() {}
+function load_components_header() {
+    wp_dequeue_style( 'wp-block-library' );
+    wp_deregister_script('wp-polyfill');
+    wp_deregister_script('regenerator-runtime');
+}
 add_action( 'wp_enqueue_scripts', 'load_components_header' );
 
 
