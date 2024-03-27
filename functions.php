bf064a8d4da1cd3c5afb4b0a2b190055449be947
Chano <github@chanovera.com>
2024-03-27 13:17:07 -0600

    limpiando head

--- functions.php
+++ functions.php
@@ -1,9 +1,9 @@
 <?php
 
 // carga componentes (estilos, javascript, etc) en el header
-function load_parts_header() {
-    // Estilos globales
-    wp_register_style( 'global', get_template_directory_uri() . '/style.css', '', 1, 'all' );
-    wp_enqueue_style( 'global' );
+function load_components_header() {
+    wp_dequeue_style( 'wp-block-library' );
+    wp_deregister_script('wp-polyfill');
+    wp_deregister_script('regenerator-runtime');
 }
-add_action( 'wp_enqueue_scripts', 'load_parts_header' );
+add_action( 'wp_enqueue_scripts', 'load_components_header' );
